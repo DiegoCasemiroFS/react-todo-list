9f2d6ffe6db6693c4c63ccf855cede0ec9e0c8e5
Diego Casemiro <diegocasemirodev@gmail.com>
2025-11-24 11:17:56 -0300

    add TextInput component and integrate it into the dialog form

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { SubHeading } from "./components/SubHeading"
 import { ToDoItem } from "./components/ToDoItem"
 import { ToDoList } from "./components/ToDoList"
 import { useState } from "react"
+import { TextInput } from "./components/TextInput"
 
 const todos = [
   {
@@ -85,7 +86,9 @@ function App() {
           </ToDoList>
           <Footer>
             <Dialog isOpen={showDialog} onClose={toogleDialog}>
-              <p>This modal dialog has a groovy backdrop!</p>
+              <form>
+                <TextInput placeholder='Digite o item que deseja adicionar' />
+              </form>
             </Dialog>
             <FabButton onClick={toogleDialog}>
               <IconPlus />
